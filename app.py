492c0a2a618c12eb801121369510e7f366b4a630
karlo <39162639+KM-BD@users.noreply.github.com>
2023-10-25 15:48:19 +0800

    Updated navbar
    Only included those we might need for the project

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('home/index.html')
+
+@app.route('/analysis.html')
+def analysis():
+    return render_template("home/analysis.html")
+
+@app.route('/tables.html')
+def tables():
+    return render_template("home/tables.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
