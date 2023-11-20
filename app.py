b4923a287b4b0c91600d7961e1d9f289661cef4b
awfusy <44151332+awfusy@users.noreply.github.com>
2023-11-20 23:00:11 +0800

    clean and remove unwanted code

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('home/index.html')
 
-@app.route('/analysis.html')
-def analysis():
-    return render_template("home/analysis.html")
-
-@app.route('/tables.html')
-def tables():
-    return render_template("home/tables.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
